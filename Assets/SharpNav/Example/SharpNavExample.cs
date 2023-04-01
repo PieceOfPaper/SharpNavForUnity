9fa70f64a91243d10b146103cf403fba7f67381c
이성희 <the.paper@live.co.kr>
2023-04-01 21:06:49 +0900

    샘플 개발

--- Assets/SharpNav/Example/SharpNavExample.cs
+++ Assets/SharpNav/Example/SharpNavExample.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SharpNav;
 
 public class SharpNavExample : MonoBehaviour
 {
@@ -11,27 +12,109 @@ public class SharpNavExample : MonoBehaviour
     //3. SharpNav.NavMesh 저장 및 불러오기 => 이 부분은 기존 코드 활용 못하니, 새로 만들어줘야한다.
 
     private SharpNav.NavMesh navMesh;
+    private List<SharpNav.Geometry.Triangle3> triangles = new List<SharpNav.Geometry.Triangle3>();
 
     // Start is called before the first frame update
     private void Start()
     {
-        //prepare the geometry from your mesh data
-        var tris = SharpNav.Geometry.TriangleEnumerable.FromIndexedVector3(null, null, 0, 1, 0, 0);
+        triangles.Clear();
+        var meshRenderers = GameObject.FindObjectsOfType<MeshRenderer>();
+        var combineInstList = new List<CombineInstance>();
+        foreach (var meshRenderer in meshRenderers)
+        {
+            if (meshRenderer == null) continue;
+            // if (UnityEditor.GameObjectUtility.AreStaticEditorFlagsSet(meshRenderer.gameObject, UnityEditor.StaticEditorFlags.NavigationStatic) == false) continue;
+            
+            var meshFilter = meshRenderer.GetComponent<MeshFilter>();
+            if (meshFilter == null) continue;
 
-        //use the default generation settings
-        var settings = SharpNav.NavMeshGenerationSettings.Default;
-        settings.AgentHeight = 1.7f;
-        // settings.AgentWidth = 0.6f;
+            var combineInst = new CombineInstance();
+            combineInst.mesh = meshFilter.mesh;
+            combineInst.transform = meshFilter.transform.localToWorldMatrix;
+            combineInstList.Add(combineInst);
+        }
+
+        var resultMesh = new Mesh();
+        resultMesh.CombineMeshes(combineInstList.ToArray(), true, true, false);
+        triangles.AddRange(resultMesh.ToSharpNavTriangles());
+
 
         //generate the mesh
-        navMesh = SharpNav.NavMesh.Generate(tris, settings);
+        var setting = NavMeshGenerationSettings.Default;
+        setting.AgentRadius = 0.2f;
+        navMesh = NavMesh.Generate(triangles, setting);
+
+        // var query = new SharpNav.NavMeshQuery(navMesh, 2048);
     }
-    
+
     private void OnDrawGizmos()
     {
+        Gizmos.color = Color.yellow;
+        foreach (var triangle in triangles)
+        {
+            var v1 = triangle.A.ToUnityVector3();
+            var v2 = triangle.B.ToUnityVector3();
+            var v3 = triangle.C.ToUnityVector3();
+
+            Gizmos.DrawLine(v1, v2);
+            Gizmos.DrawLine(v2, v3);
+            Gizmos.DrawLine(v3, v1);
+            Gizmos.DrawSphere(v1, 0.1f);
+            Gizmos.DrawSphere(v2, 0.1f);
+            Gizmos.DrawSphere(v3, 0.1f);
+        }
+
+        Gizmos.color = Color.red;
         if (navMesh != null)
         {
-            //TODO - 네브메시를 그려보자.
+            for (int tileY = 0; tileY < navMesh.TileHeight; tileY++)
+            {
+                for (int tileX = 0; tileX < navMesh.TileWidth; tileX++)
+                {
+                    var tile = navMesh.GetTileAt(tileX, tileY, 0);
+                    if (tile == null || tile.Polys == null) continue;
+                    for (int polyIndex = 0; polyIndex < tile.Polys.Length; polyIndex++)
+                    {
+                        // for (int vertIndex = 0; (vertIndex + 2) < tile.Polys[polyIndex].Verts.Length; vertIndex += 3)
+                        // {
+                        //     int vertIndex0 = tile.Polys[polyIndex].Verts[vertIndex];
+                        //     int vertIndex1 = tile.Polys[polyIndex].Verts[vertIndex + 1];
+                        //     int vertIndex2 = tile.Polys[polyIndex].Verts[vertIndex + 2];
+
+                        //     var v1 = tile.Verts[vertIndex0].ToUnityVector3();
+                        //     var v2 = tile.Verts[vertIndex1].ToUnityVector3();
+                        //     var v3 = tile.Verts[vertIndex2].ToUnityVector3();
+
+                        //     Gizmos.DrawLine(v1, v2);
+                        //     Gizmos.DrawLine(v2, v3);
+                        //     Gizmos.DrawLine(v3, v1);
+                        //     Gizmos.DrawSphere(v1, 0.1f);
+                        //     Gizmos.DrawSphere(v2, 0.1f);
+                        //     Gizmos.DrawSphere(v3, 0.1f);
+                        // }
+                        for (int vertIndex = 2; vertIndex < SharpNav.Pathfinding.PathfindingCommon.VERTS_PER_POLYGON; vertIndex++)
+                        {
+                            if (tile.Polys[polyIndex].Verts[vertIndex] == 0)
+                                break;
+
+                            int vertIndex0 = tile.Polys[polyIndex].Verts[0];
+                            int vertIndex1 = tile.Polys[polyIndex].Verts[vertIndex - 1];
+                            int vertIndex2 = tile.Polys[polyIndex].Verts[vertIndex];
+
+                            var v1 = tile.Verts[vertIndex0].ToUnityVector3();
+                            var v2 = tile.Verts[vertIndex1].ToUnityVector3();
+                            var v3 = tile.Verts[vertIndex2].ToUnityVector3();
+
+                            Gizmos.DrawLine(v1, v2);
+                            Gizmos.DrawLine(v2, v3);
+                            Gizmos.DrawLine(v3, v1);
+                            Gizmos.DrawSphere(v1, 0.1f);
+                            Gizmos.DrawSphere(v2, 0.1f);
+                            Gizmos.DrawSphere(v3, 0.1f);
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- Assets/SharpNav/Scripts/SharpNavUtility.cs
+++ Assets/SharpNav/Scripts/SharpNavUtility.cs
@@ -5,30 +5,72 @@ namespace SharpNav
 {
     public static class UnityUtility
     {
+        public static UnityEngine.Vector2 ToUnityVector2(this SharpNav.Geometry.Vector2 vector2) => new UnityEngine.Vector2(vector2.X, vector2.Y);
+        public static UnityEngine.Vector3 ToUnityVector3(this SharpNav.Geometry.Vector3 vector3) => new UnityEngine.Vector3(vector3.X, vector3.Y, vector3.Z);
+
+
         public static SharpNav.Geometry.Vector2 ToSharpNavVector3(this UnityEngine.Vector2 vector3) => new SharpNav.Geometry.Vector2(vector3.x, vector3.y);
         public static SharpNav.Geometry.Vector3 ToSharpNavVector3(this UnityEngine.Vector3 vector3) => new SharpNav.Geometry.Vector3(vector3.x, vector3.y, vector3.z);
 
         public static IEnumerable<SharpNav.Geometry.Triangle3> ToSharpNavTriangles(this UnityEngine.MeshFilter meshFilter) => meshFilter == null ? null : ToSharpNavTriangles(meshFilter.transform, meshFilter.mesh);
         public static IEnumerable<SharpNav.Geometry.Triangle3> ToSharpNavTriangles(this UnityEngine.Mesh mesh) => ToSharpNavTriangles(null, mesh);
-
         public static IEnumerable<SharpNav.Geometry.Triangle3> ToSharpNavTriangles(UnityEngine.Transform transform, UnityEngine.Mesh mesh) => mesh == null ? null : ToSharpNavTriangles(transform, mesh.vertices, mesh.GetIndices(0));
+
+        public static IEnumerable<SharpNav.Geometry.Triangle3> ToSharpNavTriangles(this UnityEngine.Terrain terrain) => terrain == null ? null : ToSharpNavTriangles(terrain.transform, terrain.terrainData);
+        public static IEnumerable<SharpNav.Geometry.Triangle3> ToSharpNavTriangles(this UnityEngine.TerrainData terrainDaata) => ToSharpNavTriangles(null, terrainDaata);
+        public static IEnumerable<SharpNav.Geometry.Triangle3> ToSharpNavTriangles(UnityEngine.Transform transform, UnityEngine.TerrainData terrainData)
+        {
+            int resolution = terrainData.heightmapResolution;
+            var meshScale = terrainData.size / (resolution - 1);
+
+            var vertices = new UnityEngine.Vector3[resolution * resolution];
+            var indices = new int[(resolution - 1) * (resolution - 1) * 6];
+
+            float[,] heights = terrainData.GetHeights(0, 0, resolution, resolution);
+
+            for (int z = 0, i = 0; z < resolution; z++)
+            {
+                for (int x = 0; x < resolution; x++, i++)
+                {
+                    vertices[i] = new UnityEngine.Vector3(x * meshScale.x, heights[z, x] * meshScale.y, z * meshScale.z);
+                    if ((i * 6 + 5) < indices.Length)
+                    {
+                        int topLeft = z * resolution + x;
+                        int bottomLeft = (z + 1) * resolution + x;
+                        int bottomRight = (z + 1) * resolution + x + 1;
+                        int topRight = z * resolution + x + 1;
+
+                        indices[i * 6] = topLeft;
+                        indices[i * 6 + 1] = bottomRight;
+                        indices[i * 6 + 2] = bottomLeft;
+                        indices[i * 6 + 3] = topLeft;
+                        indices[i * 6 + 4] = topRight;
+                        indices[i * 6 + 5] = bottomRight;
+                    }
+                }
+            }
+
+            return ToSharpNavTriangles(transform, vertices, indices);
+        }
+
+
         public static IEnumerable<SharpNav.Geometry.Triangle3> ToSharpNavTriangles(UnityEngine.Transform transform, UnityEngine.Vector3[] vertices, int[] indices)
         {
             if (vertices == null || indices == null)
                 return null;
 
-            var localToWorldMatrix = transform == null ? default : transform.localToWorldMatrix;
+            var matrix = transform == null ? default : transform.localToWorldMatrix;
             var sharpNavVertices = new SharpNav.Geometry.Vector3[vertices.Length];
             for (int i = 0; i < vertices.Length; i++)
             {
-                sharpNavVertices[i] = (transform == null ? vertices[i] : localToWorldMatrix.MultiplyPoint3x4(vertices[i])).ToSharpNavVector3();
+                sharpNavVertices[i] = (transform == null ? vertices[i] : matrix.MultiplyPoint3x4(vertices[i])).ToSharpNavVector3();
             }
 
             // var triangles = SharpNav.Geometry.TriangleEnumerable.FromIndexedVector3(sharpNavVertices, indices, 0, 1, 0, indices.Length / 3);
             var triangles = new SharpNav.Geometry.Triangle3[indices.Length / 3];
             for (int i = 0; i < triangles.Length; i ++)
             {
-                triangles[i] = new SharpNav.Geometry.Triangle3(sharpNavVertices[indices[i]], sharpNavVertices[indices[i + 1]], sharpNavVertices[indices[i + 2]]);
+                triangles[i] = new SharpNav.Geometry.Triangle3(sharpNavVertices[indices[i * 3]], sharpNavVertices[indices[i * 3 + 1]], sharpNavVertices[indices[i * 3 + 2]]);
             }
 
             return triangles;
