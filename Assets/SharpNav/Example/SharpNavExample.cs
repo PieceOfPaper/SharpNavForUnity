9300c85f0f6e7b4fe653a4c4d26cc6938c90386e
이성희 <the.paper@live.co.kr>
2023-04-01 14:09:03 +0900

    프로젝트 구성

---
+++ Assets/SharpNav/Example/SharpNavExample.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharpNavExample : MonoBehaviour
+{
+    //TODO
+    //Example을 여러 단계로 나누어서 쓰자.
+    //1. SharpNav.NavMesh 생성
+    //2. SharpNav.NavMesh로 경로탐색, Raycast 등등
+    //3. SharpNav.NavMesh 저장 및 불러오기
+    
+    private SharpNav.NavMesh navMesh;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //prepare the geometry from your mesh data
+        var tris = SharpNav.Geometry.TriangleEnumerable.FromIndexedVector3(null, null, 0, 1, 0, 0);
+
+        //use the default generation settings
+        var settings = SharpNav.NavMeshGenerationSettings.Default;
+        settings.AgentHeight = 1.7f;
+        // settings.AgentWidth = 0.6f;
+
+        //generate the mesh
+        navMesh = SharpNav.NavMesh.Generate(tris, settings);
+    }
+    
+    private void OnDrawGizmos()
+    {
+        if (navMesh != null)
+        {
+            //TODO - 네브메시를 그려보자.
+        }
+    }
+}
