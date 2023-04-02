9c9c9acf142088cbb1b802b92ef146deefc29419
이성희 <the.paper@live.co.kr>
2023-04-02 23:00:41 +0900

    Container로 묶자

--- Assets/SharpNav/Example/SharpNavExample.cs
+++ Assets/SharpNav/Example/SharpNavExample.cs
@@ -16,107 +16,22 @@ public class SharpNavExample : MonoBehaviour
     //2. SharpNav.NavMesh로 경로탐색, Raycast 등등
     //3. SharpNav.NavMesh 저장 및 불러오기 => 이 부분은 기존 코드 활용 못하니, 새로 만들어줘야한다.
 
-    private SharpNav.TiledNavMesh navMesh;
-    private SharpNav.Pathfinding.Path path;
-    private List<Vector3> smoothPath = new List<Vector3>(2048);
+    private SharpNavMesh navMesh;
+    private Vector3[] path;
 
     // Start is called before the first frame update
     private void Start()
     {
-        navMesh = navAsset == null ? null : new SharpNav.IO.Json.NavMeshJsonSerializer().DeserializeFromBinary(navAsset.bytes);
-
-        var query = new SharpNav.NavMeshQuery(navMesh, 2048);
-
-        if (startPoint != null && dstPoint != null)
-        {
-            var startPos = startPoint.position.ToSharpNavVector3();
-            var dstPos = dstPoint.position.ToSharpNavVector3();
-            var startNearlestNavPoint = query.FindNearestPoly(startPos, extends.ToSharpNavVector3());
-            var dstNearlestNavPoint = query.FindNearestPoly(dstPos, extends.ToSharpNavVector3());
-
-            path = new SharpNav.Pathfinding.Path();
-            var filter = new SharpNav.Pathfinding.NavQueryFilter();
-            var findPathResult = query.FindPath(ref startNearlestNavPoint, ref dstNearlestNavPoint, filter, path);
-            Debug.Log("FindPath " + findPathResult);
-
-			//find a smooth path over the mesh surface
-			int npolys = path.Count;
-			var iterPos = new SharpNav.Geometry.Vector3();
-			var targetPos = new SharpNav.Geometry.Vector3();
-			query.ClosestPointOnPoly(path[0], startPos, ref iterPos);
-			query.ClosestPointOnPoly(path[npolys - 1], dstPos, ref targetPos);
-
-            smoothPath.Clear();
-            smoothPath.Add(iterPos.ToUnityVector3());
-
-			float STEP_SIZE = 0.5f;
-			float SLOP = 0.01f;
-			while (npolys > 0 && smoothPath.Count < smoothPath.Capacity)
-			{
-				//find location to steer towards
-				var steerPos = new SharpNav.Geometry.Vector3();
-                SharpNav.Pathfinding.StraightPathFlags steerPosFlag = 0;
-				var steerPosRef = SharpNav.Pathfinding.NavPolyId.Null;
-
-				if (!SharpNav.QueryUtility.GetSteerTarget(query, iterPos, targetPos, SLOP, path, ref steerPos, ref steerPosFlag, ref steerPosRef))
-					break;
-
-				bool endOfPath = (steerPosFlag & SharpNav.Pathfinding.StraightPathFlags.End) != 0 ? true : false;
-				bool offMeshConnection = (steerPosFlag & SharpNav.Pathfinding.StraightPathFlags.OffMeshConnection) != 0 ? true : false;
-
-				//find movement delta
-				var delta = steerPos - iterPos;
-                float len = (float)System.Math.Sqrt(SharpNav.Geometry.Vector3.Dot(delta, delta));
-
-				//if steer target is at end of path or off-mesh link
-				//don't move past location
-				if ((endOfPath || offMeshConnection) && len < STEP_SIZE)
-					len = 1;
-				else
-					len = STEP_SIZE / len;
-
-				var moveTgt = new SharpNav.Geometry.Vector3();
-				SharpNav.QueryUtility.VMad(ref moveTgt, iterPos, delta, len);
-
-				//move
-				var result = new SharpNav.Geometry.Vector3();
-				var visited = new List<SharpNav.Pathfinding.NavPolyId>(16);
-				var startPoint = new SharpNav.Pathfinding.NavPoint(path[0], iterPos);
-				query.MoveAlongSurface(ref startPoint, ref moveTgt, out result, visited);
-				path.FixupCorridor(visited);
-				npolys = path.Count;
-				float h = 0;
-				query.GetPolyHeight(path[0], result, ref h);
-				result.Y = h;
-				iterPos = result;
-
-				//handle end of path when close enough
-				if (endOfPath && SharpNav.QueryUtility.InRange(iterPos, steerPos, SLOP, 1.0f))
-				{
-					//reached end of path
-					iterPos = targetPos;
-					if (smoothPath.Count < smoothPath.Capacity)
-					{
-						smoothPath.Add(iterPos.ToUnityVector3());
-					}
-					break;
-				}
-
-				//store results
-				if (smoothPath.Count < smoothPath.Capacity)
-				{
-                    smoothPath.Add(iterPos.ToUnityVector3());
-				}
-			}
-        }
+        navMesh = SharpNavMesh.Load(navAsset);
+        path = navMesh.FindPath(startPoint.position, dstPoint.position, extends);
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        if (navMesh != null)
+        if (navMesh != null && navMesh.NavMeshData != null)
         {
-            var tile = navMesh.GetTileAt(0, 0, 0);
+            var tile = navMesh.NavMeshData.GetTileAt(0, 0, 0);
             for (int polyIndex = 0; polyIndex < tile.Polys.Length; polyIndex++)
             {
                 for (int vertIndex = 2; vertIndex < SharpNav.Pathfinding.PathfindingCommon.VERTS_PER_POLYGON; vertIndex++)
@@ -143,13 +58,13 @@ public class SharpNavExample : MonoBehaviour
         }
 
         Gizmos.color = Color.green;
-        if (smoothPath.Count > 0)
+        if (path != null && path.Length > 0)
         {
-            Gizmos.DrawSphere(smoothPath[0], 0.1f);
-            for (int i = 1; i < smoothPath.Count; i++)
+            Gizmos.DrawSphere(path[0], 0.1f);
+            for (int i = 1; i < path.Length; i++)
             {
-                Gizmos.DrawLine(smoothPath[i - 1], smoothPath[i]);
-                Gizmos.DrawSphere(smoothPath[i], 0.1f);
+                Gizmos.DrawLine(path[i - 1], path[i]);
+                Gizmos.DrawSphere(path[i], 0.1f);
             }
         }
     }
---
+++ Assets/SharpNav/Scripts/SharpNavMesh.cs
@@ -0,0 +1,132 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SharpNav;
+using SharpNav.Pathfinding;
+
+/// <summary>
+/// Unity용 NavMesh Container
+/// </summary>
+public class SharpNavMesh
+{
+    private TiledNavMesh m_NavMeshData;
+    public TiledNavMesh NavMeshData => m_NavMeshData;
+
+    private NavMeshQuery m_Query;
+    private List<Vector3> m_PathList;
+
+    public SharpNavMesh(TiledNavMesh navMesh)
+    {
+        m_NavMeshData = navMesh;
+        m_Query = new NavMeshQuery(navMesh, 2048);
+        m_PathList = new List<Vector3>(2048);
+    }
+
+    public Vector3 GetNearlestPosition(Vector3 pos, Vector3 extends)
+    {
+        var navPoint = m_Query.FindNearestPoly(pos.ToSharpNavVector3(), extends.ToSharpNavVector3());
+        return navPoint.Position.ToUnityVector3();
+    }
+
+    public Vector3[] FindPath(Vector3 startPos, Vector3 dstPos, Vector3 extends)
+    {
+        var startNavPos = startPos.ToSharpNavVector3();
+        var dstNavPos = dstPos.ToSharpNavVector3();
+        var startNearlestNavPoint = m_Query.FindNearestPoly(startNavPos, extends.ToSharpNavVector3());
+        var dstNearlestNavPoint = m_Query.FindNearestPoly(dstNavPos, extends.ToSharpNavVector3());
+
+        var path = new Path();
+        var filter = new NavQueryFilter();
+        var findPathResult = m_Query.FindPath(ref startNearlestNavPoint, ref dstNearlestNavPoint, filter, path);
+
+        //find a smooth path over the mesh surface
+        int npolys = path.Count;
+        var iterPos = new SharpNav.Geometry.Vector3();
+        var targetPos = new SharpNav.Geometry.Vector3();
+        m_Query.ClosestPointOnPoly(path[0], startNavPos, ref iterPos);
+        m_Query.ClosestPointOnPoly(path[npolys - 1], dstNavPos, ref targetPos);
+
+        m_PathList.Clear();
+        m_PathList.Add(iterPos.ToUnityVector3());
+
+        float STEP_SIZE = 0.5f;
+        float SLOP = 0.01f;
+        while (npolys > 0 && m_PathList.Count < m_PathList.Capacity)
+        {
+            //find location to steer towards
+            var steerPos = new SharpNav.Geometry.Vector3();
+            StraightPathFlags steerPosFlag = 0;
+            var steerPosRef = NavPolyId.Null;
+
+            if (!SharpNav.QueryUtility.GetSteerTarget(m_Query, iterPos, targetPos, SLOP, path, ref steerPos, ref steerPosFlag, ref steerPosRef))
+                break;
+
+            bool endOfPath = (steerPosFlag & StraightPathFlags.End) != 0 ? true : false;
+            bool offMeshConnection = (steerPosFlag & StraightPathFlags.OffMeshConnection) != 0 ? true : false;
+
+            //find movement delta
+            var delta = steerPos - iterPos;
+            float len = (float)System.Math.Sqrt(SharpNav.Geometry.Vector3.Dot(delta, delta));
+
+            //if steer target is at end of path or off-mesh link
+            //don't move past location
+            if ((endOfPath || offMeshConnection) && len < STEP_SIZE)
+                len = 1;
+            else
+                len = STEP_SIZE / len;
+
+            var moveTgt = new SharpNav.Geometry.Vector3();
+            SharpNav.QueryUtility.VMad(ref moveTgt, iterPos, delta, len);
+
+            //move
+            var result = new SharpNav.Geometry.Vector3();
+            var visited = new List<NavPolyId>(16);
+            var startPoint = new NavPoint(path[0], iterPos);
+            m_Query.MoveAlongSurface(ref startPoint, ref moveTgt, out result, visited);
+            path.FixupCorridor(visited);
+            npolys = path.Count;
+            float h = 0;
+            m_Query.GetPolyHeight(path[0], result, ref h);
+            result.Y = h;
+            iterPos = result;
+
+            //handle end of path when close enough
+            if (endOfPath && SharpNav.QueryUtility.InRange(iterPos, steerPos, SLOP, 1.0f))
+            {
+                //reached end of path
+                iterPos = targetPos;
+                if (m_PathList.Count < m_PathList.Capacity)
+                {
+                    m_PathList.Add(iterPos.ToUnityVector3());
+                }
+                break;
+            }
+
+            //store results
+            if (m_PathList.Count < m_PathList.Capacity)
+            {
+                m_PathList.Add(iterPos.ToUnityVector3());
+            }
+        }
+        return m_PathList.ToArray();
+    }
+
+
+
+    public static SharpNavMesh Load(TextAsset textAsset)
+    {
+        return Load(textAsset.bytes);
+    }
+
+    public static SharpNavMesh Load(string text)
+    {
+        var navMesh = new SharpNav.IO.Json.NavMeshJsonSerializer().DeserializeFromText(text);
+        return new SharpNavMesh(navMesh);
+    }
+
+    public static SharpNavMesh Load(byte[] bytes)
+    {
+        var navMesh = new SharpNav.IO.Json.NavMeshJsonSerializer().DeserializeFromBinary(bytes);
+        return new SharpNavMesh(navMesh);
+    }
+}
